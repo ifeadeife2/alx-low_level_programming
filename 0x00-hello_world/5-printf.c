ec5899a6a3ae5117a93f276918fa996de54632de
Adeife Ifedayo <adeifeifedayo062@gmail.com>
2022-10-17 03:41:10 -0700

    Betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 /**
- *
- * * main - Prints a text
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
-
+ *  * main-entry point for the program.
+ *   *
+ *    * Return:0 if no errors, return non zero if errors
+ *     */
 int main(void)
-
 {
-
-	printf("with proper grammar, but the outcome is a piece of
-
-			art,\n");
-
-	return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
