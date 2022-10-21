3b7933a0730798d8aa5f5cf8f853a8da6ef5eee7
Adeife Ifedayo <adeifeifedayo062@gmail.com>
2022-10-20 21:41:09 -0700

    2-errors

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,18 +13,23 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a > b && a > c)
+
 	{
 	largest = a;
+
 	}
 	else if (b > c)
+
 	{
 	largest = b;
 								}
 	else
 
 	{
-	largest = c;
+		largest = c;
+
 	}
+
 
 	return (largest);
 }
